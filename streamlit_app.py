580890589de0180af060775f31123a6dbefe6c4b
Divya-Developer1 <124776378+Divya-Developer1@users.noreply.github.com>
2023-02-09 12:57:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.title('Omega 3 & Blueberry oatmeal')
+streamlit.title('Kale, Spinach & Rocket Smoothie')
+streamlit.title('Hard-Boiled , Free-Range egg')
